02000c2bf93d07834536df32068f696c2e3052e7
Peeyush <peeyush.srivastava@urbantouch.com>
2012-12-29 12:12:37 +0530

    sql file for database

---
+++ models/resources.sql
@@ -0,0 +1,48 @@
+Drop Table user;
+
+CREATE TABLE `user` (
+  `id` mediumint(9) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL DEFAULT '',
+  `firstname` varchar(255) NOT NULL DEFAULT '',
+  `lastname` varchar(255) NOT NULL DEFAULT '',
+  `middlename` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `oldpassword` varchar(255) DEFAULT NULL,
+  `primaryphone` varchar(255) DEFAULT NULL,
+  `gender` varchar(1) DEFAULT NULL,
+  `dob` date DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `isactive` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;
+
+
+Drop Table user_message;
+
+CREATE TABLE `user_message` (
+  `id` mediumint(9) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` mediumint(9) DEFAULT NULL,
+  `message` text,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+
+
+Drop Table friends;
+
+CREATE TABLE `friends` (
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `user_id` mediumint(9) DEFAULT NULL,
+  `friend_id` mediumint(9) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=119 DEFAULT CHARSET=utf8
+
+
+
+
+
+
+
